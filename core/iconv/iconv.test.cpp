69f3d3196b29e698c8752e3a073d39da382ab58c
Vladislav Zavjalov <slazav@altlinux.org>
2018-08-04 12:19:14 +0300

    core/iconv: add one more test

--- core/iconv/iconv.test.cpp
+++ core/iconv/iconv.test.cpp
@@ -8,9 +8,14 @@
 main(){
   try{
 
+    // convert from utf8 to koi8-r:
     IConv C1("UTF8", "KOI8-R");
     assert( C1.cnv("п©я─п╦п╡п╣я┌!") == "привет!");
 
+    // non-utf characters are not modified:
+    assert( C1.cnv("привет!") == "привет!");
+
+    // unknown charset:
     try { IConv C3("UTF8", "AAA"); }
     catch (Err e) { assert(e.str() == "can't do iconv conversion from UTF8 to AAA"); }
 
