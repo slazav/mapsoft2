8ec80506488f7739f87e632e67a5b97139c99b37
Vladislav Zavjalov <slazav@altlinux.org>
2018-08-04 12:38:06 +0300

    conv/conv_geo: more tests

--- core/conv/conv_geo.test.cpp
+++ core/conv/conv_geo.test.cpp
@@ -5,30 +5,41 @@
 
 main(){
   try{
-    ConvGeo cnv1("+datum=WGS84 +proj=lonlat","+datum=WGS84 +proj=lonlat");
+    std::string proj_wgs = "+datum=WGS84 +proj=lonlat";
+    std::string proj_krass = "+ellps=krass +towgs84=28,-130,-95 +proj=tmerc +x_0=500000 +lon_0=27";
+
+    ConvGeo cnv1(proj_wgs,proj_wgs);
 
     dPoint p1(25.651054, 60.976941);
+    iPoint p1a(427091, 6763808);
+
     dPoint p2(p1);
     cnv1.frw(p2);
     assert(p1==p2);
     cnv1.bck(p2);
     assert(p1==p2);
 
-    std::string proj1="+ellps=krass +towgs84=28,-130,-95 +proj=tmerc +x_0=500000 +lon_0=27";
-
-    ConvGeo cnv2(proj1); // to wgs84
+    ConvGeo cnv2(proj_krass); // to krass->wgs84
 
     cnv2.bck(p2);
-    assert(iPoint(p2) == iPoint(427091, 6763808));
+    assert(iPoint(p2) == p1a);
     cnv2.frw(p2);
     assert(iPoint(p2*1e6) == iPoint(p1*1e6));
 
-    ConvGeo cnv3("+datum=WGS84 +proj=lonlat", proj1);
+    // wgs->krass
+    ConvGeo cnv3(proj_wgs, proj_krass);
 
     cnv3.frw(p2);
-    assert(iPoint(p2) == iPoint(427091, 6763808));
+    assert(iPoint(p2) == p1a);
     cnv3.bck(p2);
     assert(iPoint(p2*1e6) == iPoint(p1*1e6));
+
+    dLine l1;
+    iLine l1a;
+    for (int i=0; i<3; i++) {l1.push_back(p1); l1a.push_back(p1a);}
+    cnv3.frw(l1);
+    assert(iLine(l1) == l1a);
+
 
   }
   catch (Err e) {
