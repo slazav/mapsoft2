b8f4d8688770807f881621be6b27c7714188e5ac
Vladislav Zavjalov <slazav@altlinux.org>
2019-04-28 10:49:42 +0300

    rename core/ -> modules/

--- core/file_ext/file_ext.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <cstring>
-
-bool
-file_ext(const std::string &fname, const char *ext){
-  int lf=fname.size();
-  int le=strlen(ext);
-  return (lf>=le) && (strncasecmp(fname.c_str() + (lf-le), ext, le)==0);
-}
--- core/file_ext/file_ext.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILETYPE_H
-#define FILETYPE_H
-
-#include <string>
-
-/* check if the file has specified extension (case insensitive) */
-bool file_ext(const std::string &fname, const char *ext);
-
-#endif
