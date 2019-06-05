687cf3ce41e70012bb7c08e54988035c47267a37
Vladislav Zavjalov <slazav@altlinux.org>
2019-06-05 10:37:25 +0300

    programs/ms2vmap: import/export vmap1

--- programs/ms2vmap/actions.h
+++ programs/ms2vmap/actions.h
@@ -2,6 +2,7 @@
 #define MS2VMAP_ACTIONS__H
 
 typedef void(action_t)(int argc, char *argv[], VMap & map);
-action_t action_import_mp, action_export_mp, action_cat_mp;
+action_t action_import_mp, action_export_mp,
+         action_import_vmap1, action_export_vmap1;
 
 #endif
