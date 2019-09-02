194307e447db2bfa2a2decc34798ea30e02ced5a
Vladislav Zavjalov <slazav@altlinux.org>
2019-09-02 09:33:27 +0300

    add programs/ms2mkref -- making map references

---
+++ programs/ms2mkref/ms2mkref.cpp
@@ -0,0 +1,64 @@
+///\cond HIDDEN (do not show this in Doxyden)
+
+#include <string>
+#include <vector>
+#include "getopt/getopt.h"
+#include "geo_data/geo_io.h"
+#include "geo_mkref/geo_mkref.h"
+#include <cstring>
+
+using namespace std;
+
+ext_option_list options;
+
+void usage(bool pod=false, ostream & S = cout){
+  string head = pod? "\n=head1 ":"\n";
+  const char * prog = "ms2mkref";
+  S << prog << " -- create a map reference\n"
+    << head << "Usage: "
+    << prog << "  <options>\n"
+  ;
+  S << head << "General options:\n";
+  print_options(options, MS2OPT_STD, S, pod);
+  S << head << "Making reference:\n";
+  print_options(options, MS2OPT_MKREF, S, pod);
+  S << head << "Output options (OziExplorer map format):\n";
+  print_options(options, MS2OPT_OUT | MS2OPT_GEO_O, S, pod);
+  throw Err();
+}
+
+
+int
+main(int argc, char *argv[]){
+  try{
+    ms2opt_add_std(options);
+    ms2opt_add_out(options);
+    ms2opt_add_mkref(options);
+    ms2opt_add_ozimap_o(options);
+
+    if (argc<2) usage();
+    vector<string> nonopt;
+    Opt O = parse_options_all(&argc, &argv, options, ~0, nonopt);
+    if (O.exists("help")) usage();
+    if (O.exists("pod"))  usage(true);
+    bool verb = O.exists("verbose");
+
+    if (nonopt.size()) usage();
+
+    // create map reference
+    GeoMap map = geo_mkref(O);
+
+    // write output file if needed
+    std::string ofile = O.get("out", "");
+    if (ofile != "") write_ozi_map(ofile, map, O);
+
+    return 0;
+  }
+
+  catch(Err e){
+    if (e.str()!="") cerr << "Error: " << e.str() << "\n";
+    return 1;
+  }
+}
+
+///\endcond
