819f72515fed740f6e6b5bde4f1b140921f9dcc4
Vladislav Zavjalov <slazav@altlinux.org>
2019-09-05 16:47:20 +0300

    modules/image_io: update image_png.test.cpp

--- modules/image_io/image_png.test.cpp
+++ modules/image_io/image_png.test.cpp
@@ -16,25 +16,14 @@ main(){
       "PNG error: not a PNG file: test_data/image.gif");
     assert(image_size_png("test_data/image.png") == iPoint(61,64));
 
-
     // load
     assert_err(image_load_png("test_data/missing"),
       "PNG error: can't open file: test_data/missing");
     assert_err(image_load_png("test_data/image.gif"),
       "PNG error: not a PNG file: test_data/image.gif");
+
     // 8bit
     Image img1 = image_load_png("test_data/image.png", 1);
-    // 32bit, rgba
-    Image img2 = image_load_png("test_data/image_rgba.png", 1);
-
-    // save
-    assert_err(image_save_png(img1, "test_data/"),
-      "PNG error: can't open file: test_data/");
-    image_save_png(img1, "test_data/image.tmp.png");
-    image_save_png(img2, "test_data/image_rgba.tmp.png"); // existing file
-
-    Image img2a = image_load_png("test_data/image_rgba.tmp.png", 1);
-
 
     assert(img1.width()==61);
     assert(img1.height()==64);
@@ -43,7 +32,10 @@ main(){
     assert(img1.get<int>(7,24) == 0xFFFF0000);
     assert(img1.get<int>(7,36) == 0xFF0000FF);
     assert(img1.get<int>(21,7) == 0x00000000);
-    assert(img2.get<int>(60,63) == 0xFFFFFFFF);
+    assert(img1.get<int>(60,63) == 0xFFFFFFFF);
+
+    // 32bit, rgba
+    Image img2 = image_load_png("test_data/image_rgba.png", 1);
 
     assert(img2.get<int>(0,0)  == 0xFFFFFFFF);
     assert(img2.get<int>(7,8)  == 0xFF000000);
@@ -55,6 +47,15 @@ main(){
     assert(img2.get<int>(38,22) == 0x7f00007f);
     assert(img2.get<int>(38,36) == 0x7f000000);
     assert(img2.get<int>(60,63) == 0xFFFFFFFF);
+
+    // save
+    assert_err(image_save_png(img1, "test_data/"),
+      "PNG error: can't open file: test_data/");
+    image_save_png(img1, "test_data/image.tmp.png");
+    image_save_png(img2, "test_data/image_rgba.tmp.png"); // existing file
+
+    // load back
+    Image img2a = image_load_png("test_data/image_rgba.tmp.png", 1);
 
     assert(img2a.get<int>(0,0)  == 0xFFFFFFFF);
     assert(img2a.get<int>(7,8)  == 0xFF000000);
