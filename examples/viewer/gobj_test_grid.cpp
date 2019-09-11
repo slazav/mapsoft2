3dcfdb715fc87ab38777552182beb1f489524441
Vladislav Zavjalov <slazav@altlinux.org>
2019-09-11 18:07:34 +0300

    examples/viewer: fix gobj_* to match new Image interface

--- examples/viewer/gobj_test_grid.cpp
+++ examples/viewer/gobj_test_grid.cpp
@@ -5,18 +5,19 @@ GObjTestGrid::GObjTestGrid(const int delay_):delay(delay_){}
 
 int
 GObjTestGrid::draw(Image &img, const iPoint &origin){
-  if (img.bpp() != 32) throw Err() << "gobj_test_tile: 32-bpp image expected";
+  if (img.type() != IMAGE_32ARGB) throw Err() <<
+    "image_classify: only 32-bpp images are supported";
 
   for (int j=0; j<img.height(); j++){
     for (int i=0; i<img.width(); i++){
-      img.set(i,j,0xFF<<24);
+      img.set32(i,j,0xFF<<24);
 
       int x=origin.x+i, y=origin.y+j;
       for (int n=256; n>1; n/=2){
         if ((x%n==0) || (y%n==0)){
           n--;
           uint32_t val = (0xFF<<24) + (n<<16) + (n<<8) + n;
-          img.set(i,j, val);
+          img.set32(i,j, val);
           break;
         }
       }
