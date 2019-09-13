d5ebc4eb71fcc35f9c4e446c34a4893731054e23
Vladislav Zavjalov <slazav@altlinux.org>
2019-09-13 19:36:27 +0300

    modules/viewer: add GObjMulti class -- combining multiple GObjs into one

---
+++ modules/viewer/gobj_multi.h
@@ -0,0 +1,89 @@
+#ifndef GOBJ_MULTI_H
+#define GOBJ_MULTI_H
+
+#include <map>
+#include <vector>
+#include "gobj.h"
+
+// Combine multiple GObj into one
+
+class GObjMulti : public GObj {
+private:
+
+  // add additional information to GObj
+  struct GObjData {
+    GObj * obj;
+    bool on; // on/off
+    sigc::connection redraw_conn;
+  };
+
+  std::multimap<int, GObjData> data;
+
+public:
+
+  GObjMulti(ConvBase & c): GObj(c){}
+
+
+  // add new object
+  void add(int depth, GObj * o){
+    if (!o) return;
+
+    stop_drawing = true;
+    auto lock = get_lock();
+
+    GObjData D;
+    D.obj = o;
+    D.on = true;
+
+    auto & sig = signal_redraw_me();
+    D.redraw_conn = o->signal_redraw_me().connect(
+      sigc::mem_fun (&sig, &sigc::signal<void, iRect>::emit));
+
+    data.emplace(depth, D);
+  }
+
+  // find an object
+  std::multimap<int, GObjData>::iterator find(GObj * o) {
+    for (auto i = data.begin(); i!=data.end(); ++i)
+      if (i->second.obj == o) return i;
+    return data.end();
+  }
+
+  // delete an object
+  void del(GObj * o){
+    if (!o) return;
+
+    stop_drawing = true;
+    auto lock = get_lock();
+
+    auto it = find(o);
+    if (it==data.end()) return;
+    it->second.redraw_conn.disconnect();
+    data.erase(it);
+  }
+
+  // draw all objects
+  int draw(const CairoWrapper & cr, const iPoint &origin) override{
+    int res = GObj::FILL_NONE;
+    for (auto const & p:data){
+      if (!p.second.on) continue;
+      int res1 = p.second.obj->draw(cr, origin);
+      if (res1 != GObj::FILL_NONE &&
+          res!=GObj::FILL_ALL) res=res1;
+    }
+    return res;
+  }
+
+  // rescale
+  void on_rescale(double k) override{
+    for (auto const & p:data) p.second.obj->rescale(k);
+  }
+
+  // set cnv
+  void on_set_cnv() override{
+    for (auto const & p:data) p.second.obj->set_cnv(cnv);
+  }
+
+};
+
+#endif
