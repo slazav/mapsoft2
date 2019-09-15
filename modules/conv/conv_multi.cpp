1e0d161dce15c8c76f10936513a71c181ee07e1c
Vladislav Zavjalov <slazav@altlinux.org>
2019-09-15 07:58:39 +0300

    modules/conv: ConvMulti: move some code to conv_multi.cpp

---
+++ modules/conv/conv_multi.cpp
@@ -0,0 +1,40 @@
+#include "conv_multi.h"
+#include "conv_aff.h"
+
+/// Try to substitude all conversions by a single ConvAff.
+//  Algorythm:
+//  - make NxN points in the src_box, convert src->dst
+//  - build ConvAff using these points
+//  - convert points back to src coordinates
+//  - measure error (mean square distance, in src coordinates)
+//  - if error < E substitude all conversions with the new one
+//  - return true if substitution was none, false otherwise.
+bool
+ConvMulti::simplify(const dRect & src_box, int N, double E) {
+  if (N<2 || N>1000) throw Err() <<
+    "ConvMulti::simplify: wrong number of points: " << N;
+  dLine pts_src;
+  for (int i=0; i<N; ++i) for (int j=0; j<N; ++j){
+    pts_src.push_back(src_box.tlc() +
+      dPoint(src_box.w*i/(N-1), src_box.h*j/(N-1)));
+  }
+  int NP = pts_src.size();
+  dLine pts_dst(pts_src);
+  frw(pts_dst);
+  std::map<dPoint, dPoint> ptmap;
+  for (int i=0; i<NP; ++i)
+    ptmap.emplace(pts_src[i], pts_dst[i]);
+
+  std::shared_ptr<ConvAff2D> new_cnv(new ConvAff2D(ptmap));
+  new_cnv->bck(pts_dst); // to src coordinates
+
+  // if error in the conversion is small, use this conversion
+  if (new_cnv->get_src_err() < E){
+    cnvs.clear();
+    dirs.clear();
+    cnvs.push_back(new_cnv);
+    dirs.push_back(true);
+    return true;
+  }
+  return false;
+}
--- modules/conv/conv_multi.h
+++ modules/conv/conv_multi.h
@@ -2,7 +2,6 @@
 #define CONV_MULTI_H
 
 #include "conv_base.h"
-#include "conv_aff.h"
 #include <list>
 #include <memory>
 
@@ -88,34 +87,7 @@ public:
   //  - measure error (mean square distance, in src coordinates)
   //  - if error < E substitude all conversions with the new one
   //  - return true if substitution was none, false otherwise.
-  bool simplify(const dRect & src_box, int N, double E = 1) {
-    if (N<2 || N>1000) throw Err() <<
-      "ConvMulti::simplify: wrong number of points: " << N;
-    dLine pts_src;
-    for (int i=0; i<N; ++i) for (int j=0; j<N; ++j){
-      pts_src.push_back(src_box.tlc() +
-        dPoint(src_box.w*i/(N-1), src_box.h*j/(N-1)));
-    }
-    int NP = pts_src.size();
-    dLine pts_dst(pts_src);
-    frw(pts_dst);
-    std::map<dPoint, dPoint> ptmap;
-    for (int i=0; i<NP; ++i)
-      ptmap.emplace(pts_src[i], pts_dst[i]);
-
-    std::shared_ptr<ConvAff2D> new_cnv(new ConvAff2D(ptmap));
-    new_cnv->bck(pts_dst); // to src coordinates
-
-    // if error in the conversion is small, use this conversion
-    if (new_cnv->get_src_err() < E){
-      cnvs.clear();
-      dirs.clear();
-      cnvs.push_back(new_cnv);
-      dirs.push_back(true);
-      return true;
-    }
-    return false;
-  }
+  bool simplify(const dRect & src_box, int N, double E = 1);
 
   int size() const {return cnvs.size();}
 
