5e46c896d8378b1d01869c3a78528fb5993293f4
Vladislav Zavjalov <slazav@altlinux.org>
2019-09-18 15:02:43 +0300

    modules/geo_render: write_geoimg: fix image size calculation

--- modules/geo_render/write_geoimg.cpp
+++ modules/geo_render/write_geoimg.cpp
@@ -45,10 +45,15 @@ write_geoimg(const std::string & fname, GeoData & data, const Opt & opts){
   GeoMap map = geo_mkref(data, opts);
   std::shared_ptr<ConvMap> cnv(new ConvMap(map));
 
+  // find image dimensions
+  dRect box = dRect(dPoint(), (dPoint)map.image_size);
+  if (box.is_zsize()) box = map.border.bbox();
+  if (box.is_zsize()) throw Err() << "write_img: can't get map dimensions";
+
   // setup cairo context
   CairoWrapper cr;
   Image img;
-  int w=map.image_size.x, h=map.image_size.y;
+  int w=box.brc().x, h=box.brc().y;
   if      (fmt == "pdf") cr.set_surface_pdf(fname.c_str(), w,h);
   else if (fmt == "ps")  cr.set_surface_ps(fname.c_str(), w,h);
   else if (fmt == "svg") cr.set_surface_svg(fname.c_str(), w,h);
@@ -73,10 +78,6 @@ write_geoimg(const std::string & fname, GeoData & data, const Opt & opts){
     obj.add(1, std::shared_ptr<GObjWpts>(new GObjWpts(w)));
 
   // draw tracks and waypoints
-  dRect box = dRect(dPoint(), (dPoint)map.image_size);
-  if (box.is_zsize()) box = map.border.bbox();
-  if (box.is_zsize()) throw Err() << "write_img: can't get map dimensions";
-
   obj.draw(cr, box);
 
 // // draw grid
