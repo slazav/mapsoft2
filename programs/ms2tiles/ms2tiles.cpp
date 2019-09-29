b9082fd064285d262cb2460ff7b01abaa640e65d
Vladislav Zavjalov <slazav@altlinux.org>
2019-09-29 16:19:25 +0300

    programs/ms2nom, ms2tiles: use HelpPrinter for printing help mesage

--- programs/ms2tiles/ms2tiles.cpp
+++ programs/ms2tiles/ms2tiles.cpp
@@ -1,27 +1,29 @@
 #include <cstring>
 #include "geo_tiles/geo_tiles.h"
+#include "getopt/getopt.h"
 
 using namespace std;
 
-void usage(){
-  cout << "\n"
-    "ms2tiles -- TMS/Google tile calculations\n"
-    "\n"
-    "usage: ms2tiles [-G] -p <point> <z> -- tiles at the point\n"
-    "       ms2tiles [-G] -r <range> <z> -- tiles covering the range\n"
-    "       ms2tiles [-G] -n <tile-point> -- tile range\n"
-    "       ms2tiles [-G] -c <tile-point> -- tile center\n"
-    "       ms2tiles [-G] -t <tile-point> <range>  -- check if the tile touches the range\n"
-    "Order of options is important.\n"
-    "\n"
-    "By default the program works with TMS tiles. Use -G option\n"
-    "for google tiles.\n"
-    "\n"
-    "Parameters <point> and <range> are JSON arrays of WGS84\n"
-    "coordinates: [<lon>,<lat>], and [<lon>,<lat>,<width>,<height>].\n"
-    "Parameter <tile-point> is JSON array of three integers: [<x>,<y>,<z>]\n."
-    "\n"
-  ;
+void usage(bool pod=false){
+  GetOptSet options;
+  HelpPrinter pr(pod, options, "ms2tiles");
+  pr.name("TMS/Google tile calculations");
+  pr.usage("ms2tiles [-G] -p <point> <z> -- tiles at the point");
+  pr.usage("ms2tiles [-G] -r <range> <z> -- tiles covering the range");
+  pr.usage("ms2tiles [-G] -n <tile-point> -- tile range");
+  pr.usage("ms2tiles [-G] -c <tile-point> -- tile center");
+  pr.usage("ms2tiles [-G] -t <tile-point> <range>  -- check if the tile touches the range");
+
+  pr.par("Order of options is important.");
+  pr.par(
+    "By default the program works with TMS tiles. Use -G option "
+    "for google tiles."
+  );
+  pr.par(
+    "Parameters <point> and <range> are JSON arrays of WGS84 "
+    "coordinates: [<lon>,<lat>], and [<lon>,<lat>,<width>,<height>]. "
+    "Parameter <tile-point> is JSON array of three integers: [<x>,<y>,<z>]."
+  );
 }
 
 int
