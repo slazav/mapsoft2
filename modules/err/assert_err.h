d3c5746ae9179f96a134795ce12bd398753d459d
Vladislav Zavjalov <slazav@altlinux.org>
2019-10-10 08:44:10 +0300

    modules/err: more information is printed by assert_err and assert_eq

--- modules/err/assert_err.h
+++ modules/err/assert_err.h
@@ -14,12 +14,14 @@
     cmd;\
     throw Err(-9999)\
       << "assert_err: " << __FILE__ << ":" << __LINE__ << ": error is not thrown:\n"\
-      << "expected error: [" << (ret)<< "]\n";\
+      << "command: " << #cmd << "\n"\
+      << "expected error: " << (ret)<< "\n";\
   } catch (Err e) {\
     if (e.code()==-9999) throw e;\
     if (e.str()!=(ret)){\
       throw Err()\
         << "assert_err: " << __FILE__ << ":" << __LINE__ << ": wrong error message:\n"\
+        << "command: " << #cmd << "\n"\
         << "expected error: " << (ret) << "\n"\
         << "actual error:   " << e.str()<< "\n";\
     }\
--- modules/err/assert_err.test.cpp
+++ modules/err/assert_err.test.cpp
@@ -12,13 +12,20 @@ void nonerrorfunc(){}
 
 int
 main(){
+  try{
+    assert_err(errorfunc(), "some error");
 
-  assert_err(errorfunc(), "some error");
 //  assert_err(errorfunc(), "some error1");
 //  assert_err(nonerrorfunc(), "some error");
 
-  assert_err(throw Err() << "eee", "eee");
+    assert_err(throw Err() << "eee", "eee");
 
+  }
+  catch (Err e) {
+    std::cerr << "Error: " << e.str() << "\n";
+    return 1;
+  }
+  return 0;
 }
 
 ///\endcond
