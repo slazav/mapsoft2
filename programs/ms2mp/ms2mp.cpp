c7f444094a2a14a5b6732be32d2f97507ba970b3
Vladislav Zavjalov <slazav@altlinux.org>
2019-11-02 11:28:27 +0200

    modules/mp: move test program to programs/ms2mp

---
+++ programs/ms2mp/ms2mp.cpp
@@ -0,0 +1,51 @@
+#include <fstream>
+#include "mp/mp.h"
+#include "getopt/getopt.h"
+
+using namespace std;
+
+GetOptSet options;
+
+void usage(bool pod=false){
+
+  HelpPrinter pr(pod, options, "ms2mp");
+  pr.name("MP format converter");
+
+  pr.usage("[<options>] <in_files> > <out_file>");
+
+  pr.head(1, "Options");
+  pr.opts({"OUT", "MP"});
+  throw Err();
+}
+
+int
+main(int argc, char **argv){
+  try {
+
+    ms2opt_add_out(options);
+    ms2opt_add_mp(options);
+    options.remove("verbose");
+
+    if (argc<2) usage();
+    vector<string> files;
+    Opt O = parse_options_all(&argc, &argv, options, {}, files);
+    if (O.exists("help")) usage();
+    if (O.exists("pod"))  usage(true);
+
+
+    MP data;
+    for (auto const &f: files){
+      std::ifstream s(f);
+      read_mp(s, data, O);
+    }
+
+    write_mp(std::cout, data, O);
+
+  }
+  catch (Err e) {
+    if (e.str()!="") cerr << "Error: " << e.str() << "\n";
+    return 1;
+  }
+  return 0;
+}
+
