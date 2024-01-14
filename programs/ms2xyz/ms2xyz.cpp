84c4daa037e3157f9c25aad6d2e18e426944d3d7
Vladislav Zavjalov <slazav@altlinux.org>
2024-01-14 03:56:19 +0000

    ms2xyz: rearrange code

--- programs/ms2xyz/ms2xyz.cpp
+++ programs/ms2xyz/ms2xyz.cpp
@@ -78,6 +78,143 @@ void usage(bool pod=false){
   throw Err();
 }
 
+/**********************************************************/
+struct track_pt_t{
+  GeoTpt pt;
+  double time1, time2, dt; // seconds
+  double dist1, dist2, dd; // meters
+  double speed;            // km/h
+  int    nn1, nn2, pn;
+  bool   br; // print linebreak before the point
+
+  // print values according to fmt
+  void print(const Opt & O) const{
+    string tfmt   = O.get<string>("tfmt", "%F %T");
+    string fmt    = O.get<string>("fmt", "%x %y %z %T %D %S");
+    int llprec    = O.get<int>("llprec", 7);
+    int zprec     = O.get<int>("zprec", 1);
+    int dprec     = O.get<int>("dprec", 3);
+    int sprec     = O.get<int>("sprec", 2);
+    int tprec     = O.get<int>("tprec", 1);
+
+    if (br) cout << endl;
+    bool esc=false;
+    for (string::const_iterator c=fmt.begin(); c!=fmt.end(); c++){
+      if (!esc){
+        if (*c=='%') esc=true;
+        else cout << *c;
+        continue;
+      }
+      switch(*c){
+        case '%': cout << *c; break;
+        case 'x': cout << fixed << setprecision(llprec) << pt.x; break;
+        case 'y': cout << fixed << setprecision(llprec) << pt.y; break;
+        case 'z': cout << fixed << setprecision(zprec) << pt.z; break;
+
+        case 't': cout << int(pt.t/1000.0); break;
+        case 'T': cout << write_fmt_time(tfmt.c_str(), pt.t); break;
+        case 'U': cout << fixed << setprecision(tprec) << time1; break;
+        case 'u': cout << fixed << setprecision(tprec) << time2; break;
+        case 'v': cout << fixed << setprecision(tprec) << dt;    break;
+
+        case 'D': cout << fixed << setprecision(dprec) << dist1/1000.0; break;
+        case 'd': cout << fixed << setprecision(dprec) << dist2/1000.0; break;
+        case 'e': cout << fixed << setprecision(dprec) << dd; break;
+
+        case 'S': cout << fixed << setprecision(sprec) << speed;    break;
+
+        case 'N': cout << nn1;    break;
+        case 'n': cout << nn2;    break;
+        case 'p': cout << pn+1;    break;
+        default:
+          cout << '%' << *c;
+      }
+      esc=false;
+    }
+    cout << endl;
+  }
+};
+/**********************************************************/
+
+std::vector<track_pt_t>
+read_data(const GeoData & data, const Opt & O){
+  double tshift = O.get<double>("tshift", 0);
+  double window = O.get<double>("window", 120.0);
+  string br     = O.get<string>("break", "none");
+  std::vector<track_pt_t> pt_data;
+
+  double speed=0, dist = 0, Dist = 0, t0 = 0, T0 = -1;
+  queue<pair<double, double> > timedist;
+
+  size_t nn = 0;
+  size_t NN = 0;
+  size_t pn = 0;
+  GeoTpt tp, pp;
+  for (auto const & trk: data.trks) {
+    for (size_t p = 0; p < trk.size(); ++p) {
+
+      tp = trk[p];
+      tp.t += tshift*3600*1000;
+
+      if (T0<0) T0 = tp.t;
+
+      bool do_br = false;
+      if ( (br == "track" && (tp.start || p==0)) ||
+           (br == "day"   && (nn != 0) &&
+             (tp.t/86400000 != pp.t/86400000)) ){
+        nn=0;
+        pn++;
+        do_br = true;
+      }
+
+      double dd=0;
+      int    dt=0;
+      if (nn != 0) {
+        // update values:
+        dd = geo_dist_2d(tp,pp);
+        dt = tp.t - pp.t;
+        dist +=dd;
+        Dist +=dd;
+      } else {
+        dist = 0;
+        t0 = tp.t;
+      }
+
+      timedist.push(make_pair(tp.t/1000.0, Dist));
+      while (timedist.front().first < tp.t/1000.0 - window &&
+             timedist.size() > 2) timedist.pop();
+
+      double traveled = timedist.back().second - timedist.front().second;
+      double time = timedist.back().first - timedist.front().first;
+      if (time==0) speed=0;
+      else speed = traveled/time * 3.6;
+
+      pp = tp;
+      ++nn;
+      ++NN;
+
+      // fill data
+      track_pt_t pt;
+      pt.pt = tp;
+      pt.time1 = (tp.t-T0)/1000.0;
+      pt.time2 = (tp.t-t0)/1000.0;
+      pt.dt = dt/1000.0;
+      pt.dist1 = Dist;
+      pt.dist2 = dist;
+      pt.dd = dd;
+      pt.speed = speed;
+      pt.nn1 = NN;
+      pt.nn2 = nn;
+      pt.pn = pn;
+      pt.br = do_br;
+      pt_data.push_back(pt);
+    }
+  }
+
+  return pt_data;
+}
+
+/**********************************************************/
 
 int
 main (int argc, char **argv) {
@@ -116,110 +253,17 @@ main (int argc, char **argv) {
     if (O.exists("help")) usage();
     if (O.exists("pod"))  usage(true);
 
-    GeoData data;
-    for (auto const & f:infiles) read_geo(f, data, O);
-
-    double tshift = O.get<double>("tshift", 0);
-    string tfmt   = O.get<string>("tfmt", "%F %T");
-    string fmt    = O.get<string>("fmt", "%x %y %z %T %D %S");
-    double window = O.get<double>("window", 120.0);
-    string br     = O.get<string>("break", "none");
-    int llprec    = O.get<int>("llprec", 7);
-    int zprec     = O.get<int>("zprec", 1);
-    int dprec     = O.get<int>("dprec", 3);
-    int sprec     = O.get<int>("sprec", 2);
-    int tprec     = O.get<int>("tprec", 1);
-
-    double speed=0, dist = 0, Dist = 0, t0 = 0, T0 = -1;
-    queue<pair<double, double> > timedist;
-
+    // open output file
     string ofile = O.get("out","");
     if (ofile != "") FILE * F=freopen(ofile.c_str(),"w",stdout);
 
-    size_t nn = 0;
-    size_t NN = 0;
-    size_t pn = 0;
-    GeoTpt tp, pp;
-    for (auto const & trk: data.trks) {
-      for (size_t p = 0; p < trk.size(); ++p) {
+    // read data
+    GeoData data;
+    for (auto const & f:infiles) read_geo(f, data, O);
+    auto pt_data = read_data(data, O);
 
-        tp = trk[p];
-        tp.t += tshift*3600*1000;
-
-        if (T0<0) T0 = tp.t;
-
-        if ( (br == "track" && (tp.start || p==0)) ||
-             (br == "day"   && (nn != 0) &&
-               (tp.t/86400000 != pp.t/86400000)) ){
-          nn=0;
-          pn++;
-          cout << endl;
-        }
-
-        double dd=0;
-        int    dt=0;
-        if (nn != 0) {
-          // update values:
-          dd = geo_dist_2d(tp,pp);
-          dt = tp.t - pp.t;
-          dist +=dd;
-          Dist +=dd;
-        } else {
-          dist = 0;
-          t0 = tp.t;
-        }
-
-        timedist.push(make_pair(tp.t/1000.0, Dist));
-        while (timedist.front().first < tp.t/1000.0 - window &&
-               timedist.size() > 2) timedist.pop();
-
-        double traveled = timedist.back().second - timedist.front().second;
-        double time = timedist.back().first - timedist.front().first;
-        if (time==0) speed=0;
-        else speed = traveled/time * 3.6;
-
-        pp = tp;
-        ++nn;
-        ++NN;
-
-        // print values according to fmt
-        bool esc=false;
-        for (string::const_iterator c=fmt.begin(); c!=fmt.end(); c++){
-          if (!esc){
-            if (*c=='%') esc=true;
-            else cout << *c;
-            continue;
-          }
-          switch(*c){
-            case '%': cout << *c; break;
-            case 'x': cout << fixed << setprecision(llprec) << tp.x; break;
-            case 'y': cout << fixed << setprecision(llprec) << tp.y; break;
-            case 'z': cout << fixed << setprecision(zprec) << tp.z; break;
-
-            case 't': cout << int(tp.t/1000.0); break;
-            case 'T': cout << write_fmt_time(tfmt.c_str(), tp.t); break;
-            case 'u': cout << fixed << setprecision(tprec) << (tp.t-t0)/1000.0; break;
-            case 'U': cout << fixed << setprecision(tprec) << (tp.t-T0)/1000.0; break;
-            case 'v': cout << fixed << setprecision(tprec) << dt/1000.0; break;
-
-            case 'd': cout << fixed << setprecision(dprec) << dist/1000.0; break;
-            case 'D': cout << fixed << setprecision(dprec) << Dist/1000.0; break;
-            case 'e': cout << fixed << setprecision(dprec) << dd; break;
-
-            case 'S': cout << fixed << setprecision(sprec) << speed;    break;
-
-            case 'n': cout << nn;    break;
-            case 'N': cout << NN;    break;
-            case 'p': cout << pn+1;    break;
-            default:
-              cout << '%' << *c;
-          }
-          esc=false;
-        }
-        cout << endl;
-
-      }
-    }
+    // print data
+    for (const auto & pt : pt_data) pt.print(O);
   }
   catch (Err & e) {
     if (e.str()!="") std::cerr << "Error: " << e.str() << "\n";
