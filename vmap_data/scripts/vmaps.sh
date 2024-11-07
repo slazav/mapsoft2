4b7daf6bbde3c8022aa34f22ddcc004354545bdc
Vladislav Zavjalov <slazav@altlinux.org>
2024-11-07 11:20:17 +0200

    vmap_data/scripts: Move code for processing individual maps from vmaps_out
    vmaps_png -- create png + map for specified maps or for all new maps
    vmaps_img -- same for mp.gz + img files
    vmaps_htm -- same for htm + jpg files (to be removed)
    vmaps_tiles -- same for tiles
    
    All tiles accept map names as arguments or update only new maps in there are no arguments
    Note that now tiles are created based on timestamps in $TILE_DIR/<name>.tstamp files

--- vmap_data/scripts/vmaps.sh
+++ vmap_data/scripts/vmaps.sh
@@ -1,18 +1,21 @@
 # configuration file for vector map factory
 
+# directories:
 VMAP_DIR=vmap;   # Source folder (vmap/vmap2)
 IN_DIR=IN;       # Input folder. Maps are updated from .fig/.mp files in this folder
 OUT_DIR=OUT;     # Output folder for raster maps, mp/img files
 DIFF_DIR=diff;   # Folder for diff files
 FIG_DIR=fig;     # Fig templates
-BRD_DIR=brd;     # Global borders in gpx format -- for tiles
-BRD2_DIR=brd;    # Global borders in gpx format -- for index files
-CROP_NOM=0;      # crop maps in vmaps_in (only for nomenclature maps)
+BRD_DIR=brd;     # map borders in gpx format
+BRD2_DIR=brd;    # borders for region index files (to be removed?)
 
+CROP_NOM=0;      # crop maps in vmaps_in (only for nomenclature maps)
 VMAP_EXT=vmap;       # Extension/format of vector maps
 NOM_MAG=2;           # Scaling of maps. Use NOM_MAP=2 for 1km sheets with 500m maps
 DPI=400;             # DPI for rendering images
 DPI_PR=150;          # DPI for rendering preview images
+
+# render parameters:
 DPI_MAP=200;         # DPI for "original" map reference (normally 200 or 100)
 STYLE_HR=0;          # hr variable to be used in render.cfg and types.cfg
 STYLE_V2_PER=0;      # new style of passes
@@ -22,13 +25,17 @@ CMAP=conf/cmap.png;        # Colormap
 CMAP_SRC=                  # nomenclatere name used for colormap source
 REND_CFG=/usr/share/mapsoft2/render.cfg;  # Render configuration
 TYPEINFO=/usr/share/mapsoft2/types.cfg    # Type information
-HTM_TEMPL=/usr/share/mapsoft2/map_templ.htm;    # template for htm page
+HTM_TEMPL=/usr/share/mapsoft2/map_templ.htm;    # template for htm page (to be removed)
 
-# index files (one set per BRD2_DIR/*.gpx)
+# generating index files:
 TYPSRC=/usr/share/mapsoft2/typ.txt  # typ file source for garmin IMG
 EXTRA_TRACKS=;    # extra tracks to be added to index image
 JPEG_SCALE=0.2;   # scale for jpeg preview images
 INDEX_SCALE=0.05; # scale for index image (in addition to jpeg_scale)
+
+# new index files (not used yet)
+INDEX_TXT=$OUT_DIR/index.txt
+INDEX_HTM=$OUT_DIR/index.htm
 
 # tiles (single set for all maps)
 TILE_DIR=TILES;
@@ -46,6 +53,8 @@ MS2RENDER=ms2render
 MS2GEOFIG=ms2geofig
 MS2CONV=ms2conv
 MS2NOM=ms2nom
+GMT=gmt
+CGPSM=cgpsmapper-static
 
 function vmap_defs() {
   name="$1"
@@ -76,11 +85,68 @@ function vmap_render_map() {
   title=$5
   dpi=${6:-$DPI}
   $MS2RENDER $ifile --out "$png"\
+    $BRD_DIR/*.gpx --trk_draw_transp 0.5 --trk_draw_dots 0 --trk_draw_width 3\
     --config "$REND_CFG" -t "$TYPEINFO" --define "$(vmap_defs "$name")"\
     --mkref nom --north --name "$name" --dpi "$dpi" --margins 10 --top_margin 30\
     --title "$title" --title_size 20\
     --cmap_load "$CMAP" --png_format pal ${map:+--map $map}
 }
+
+# list all vmaps which are newer then png or map
+function list_vmap_nt_png {
+  for vmap in $(find $VMAP_DIR -name "*.$VMAP_EXT" | sort); do
+    png="$OUT_DIR/$(basename $vmap .$VMAP_EXT).png"
+    map="$OUT_DIR/$(basename $vmap .$VMAP_EXT).map"
+    [ "$vmap" -nt "$png" -o "$vmap" -nt "$map" ] && echo "$vmap" ||:
+  done
+}
+
+# list all vmaps which are newer then img or mp.zip
+function list_vmap_nt_img {
+  for vmap in $(find $VMAP_DIR -name "*.$VMAP_EXT" | sort); do
+    img="$OUT_DIR/$(basename $vmap .$VMAP_EXT).img"
+    mpz="$OUT_DIR/$(basename $vmap .$VMAP_EXT).mp.zip"
+    [ "$vmap" -nt "$img" -o "$vmap" -nt "$mpz" ] && echo "$vmap" ||:
+  done
+}
+
+# list all vmaps which are newer then htm or jpg (to be removed?)
+function list_vmap_nt_htm {
+  for vmap in $(find $VMAP_DIR -name "*.$VMAP_EXT" | sort); do
+    jpg="$OUT_DIR/$(basename $vmap .$VMAP_EXT).jpg"
+    htm="$OUT_DIR/$(basename $vmap .$VMAP_EXT).htm"
+    [ "$vmap" -nt "$jpg" -o "$vmap" -nt "$htm" ] && echo "$vmap" ||:
+  done
+}
+
+# list all vmaps which are newer then tile tstamp file
+function list_vmap_nt_tiles {
+  for vmap in $(find $VMAP_DIR -name "*.$VMAP_EXT" | sort); do
+    tst="$TILE_DIR/$(basename $vmap .$VMAP_EXT).tstamp"
+    [ "$vmap" -nt "$tst" ] && echo "$vmap" ||:
+  done
+}
+
+
+# list all vmaps which are newer then index.txt
+function list_vmap_nt_index {
+  for vmap in $(find $VMAP_DIR -name "*.$VMAP_EXT" | sort); do
+    [ "$vmap" -nt "$INDEX_TXT" ] && echo "$vmap" ||:
+  done
+}
+
+# check if a file exists and is newer then any png
+function check_nt_png {
+  n="$(set +f; ls -1t $OUT_DIR/*.png | head -1)"
+  [ -f "$1" -a "$1" -nt "$n" ]
+}
+
+# check if a file exists and is newer then any tiles
+function check_nt_tiles {
+  n="$(set +f; ls -1t $TILE_DIR/*.tstamp | head -1)"
+  [ -f "$1" -a "$1" -nt "$n" ]
+}
+
 
 # Print date of the last commit for a given file
 function vmap_git_date {
@@ -114,3 +180,4 @@ function vmap_git_status_list {
   done
   [ "$clean" = 1 ]
 }
+
