7bcf82770148539bb2d946eba784418b2071c075
Vladislav Zavjalov <slazav@altlinux.org>
2025-01-10 14:09:08 +0200

    ms2vmap: typ-file writing (--typ option); allow zero output files

--- programs/ms2vmap/ms2vmap.cpp
+++ programs/ms2vmap/ms2vmap.cpp
@@ -43,6 +43,8 @@ void usage(bool pod=false){
   pr.opts({"VMAP"});
   pr.head(2, "Options for OSM XML format:");
   pr.opts({"OSM"});
+  pr.head(2, "Options for writing TYP file:");
+  pr.opts({"TYP"});
 
   throw Err();
 }
@@ -55,6 +57,9 @@ main(int argc, char *argv[]){
     ms2opt_add_std(options, {"HELP","POD","VERB","OUT"});
     ms2opt_add_vmap2t(options);
     ms2opt_add_vmap2(options, 1, 1);
+    options.add("typ",     1, 0, "TYP", "Write TYP file source for cgpsmapper");
+    options.add("typ_fid", 1, 0, "TYP", "FID number for TYP file (by default it's not written)");
+    options.add("typ_cp",  1, 0, "TYP", "CodePage setting for TYP file (default:1251)");
     options.remove("verbose");
 
     // general options -- up to first non-option argument
@@ -63,13 +68,12 @@ main(int argc, char *argv[]){
     if (O.exists("help")) usage();
     if (O.exists("pod"))  usage(true);
 
-    // check output file
-    std::string ofile = O.get("out", "");
-    if (ofile == "") throw Err()
-      << "non-empty output file expected (use -o option)";
-
     // read file with type information if it's available
     VMap2types types(O);
+
+    // write typ file
+    if (O.get("typ")!="")
+      types.write_typ(O.get("typ"), O.get<int>("typ_cp", 1251), O.get<int>("typ_fid", -1));
 
     // move to import? how to avoid copying of vmap?
     VMap2 vmap;
@@ -78,7 +82,7 @@ main(int argc, char *argv[]){
     else
       vmap2_import(infiles, types, vmap, O); // read all files (incl databases)
 
-    vmap2_export(vmap, types, ofile, O);
+    if (O.get("out")!="") vmap2_export(vmap, types, O.get("out"), O);
   }
   catch (Err & e) {
     if (e.str()!="") cerr << "Error: " << e.str() << "\n";
