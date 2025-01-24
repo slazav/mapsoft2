8f470d107a36d10483a54b99f42dd9a6b02689c1
Vladislav Zavjalov <slazav@altlinux.org>
2025-01-24 18:22:10 +0200

    ms2vmap: move code for writing typ file to ms2vmaptypes

--- programs/ms2vmap/ms2vmap.cpp
+++ programs/ms2vmap/ms2vmap.cpp
@@ -43,8 +43,6 @@ void usage(bool pod=false){
   pr.opts({"VMAP"});
   pr.head(2, "Options for OSM XML format:");
   pr.opts({"OSM"});
-  pr.head(2, "Options for writing TYP file:");
-  pr.opts({"TYP"});
 
   throw Err();
 }
@@ -57,9 +55,6 @@ main(int argc, char *argv[]){
     ms2opt_add_std(options, {"HELP","POD","VERB","OUT"});
     ms2opt_add_vmap2t(options);
     ms2opt_add_vmap2(options, 1, 1);
-    options.add("typ",       1, 0, "TYP", "Write TYP file source for cgpsmapper");
-    options.add("typ_fid",   1, 0, "TYP", "FID number for TYP file (by default it's not written)");
-    options.add("typ_cp",    1, 0, "TYP", "CodePage setting for TYP file (default:1251)");
     options.remove("verbose");
 
     // general options -- up to first non-option argument
@@ -70,10 +65,6 @@ main(int argc, char *argv[]){
 
     // read file with type information if it's available
     VMap2types types(O);
-
-    // write typ file
-    if (O.get("typ")!="")
-      types.write_typ(O.get("typ"), O.get<int>("typ_cp", 1251), O.get<int>("typ_fid", -1));
 
     // move to import? how to avoid copying of vmap?
     VMap2 vmap;
